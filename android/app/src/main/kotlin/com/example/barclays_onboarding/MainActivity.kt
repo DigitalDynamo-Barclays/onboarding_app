eea4c97ee0fbaaf27b58197bc89b4f3f31cbfc46
Tanishq2505 <tanishqa89@gmail.com>
2023-03-31 11:58:26 +0530

    initial commit 🎉

---
+++ android/app/src/main/kotlin/com/example/barclays_onboarding/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.barclays_onboarding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF00aeef);
+const kSecondaryColor = Color(0xFF00395d);
+const kTertiaryColor = Color(0xFFFFFFFF);
