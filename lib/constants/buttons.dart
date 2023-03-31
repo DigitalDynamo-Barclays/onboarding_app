85484c50d2f56c99796fd5ed294b6b58de82cc8a
vk2122 <varunkhachane31@gmail.com>
2023-03-31 17:56:18 +0530

    commit

---
+++ lib/constants/buttons.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Widget button1(height, width, textscale, text, color1, color2, route) {
+  return GestureDetector(
+    onTap: route,
+    child: Container(
+      height: height * 40 / 640,
+      width: width * 313 / 360,
+      decoration: BoxDecoration(
+        color: color1,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      alignment: Alignment.center,
+      child: Text(
+        text,
+        style: GoogleFonts.montserrat(
+            color: color2,
+            fontSize: textscale * 17,
+            fontWeight: FontWeight.w600),
+        textAlign: TextAlign.center,
+      ),
+    ),
+  );
+}
+
+Widget button2(height, width, textscale, text, icon, color1, color2, route) {
+  return GestureDetector(
+    onTap: route,
+    child: Container(
+      height: height * 61 / 640,
+      width: width * 357 / 360,
+      decoration: BoxDecoration(
+        color: color1,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      alignment: Alignment.center,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: color2,
+            size: textscale * 30,
+          ),
+          SizedBox(width: 10),
+          Text(
+            text,
+            style: GoogleFonts.montserrat(
+                color: color2,
+                fontSize: textscale * 18,
+                fontWeight: FontWeight.w500),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    ),
+  );
+}
---
+++ lib/screens/loading_page.dart
@@ -0,0 +1,38 @@
+import 'package:barclays_onboarding/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({super.key});
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: height * 80 / 640),
+            SizedBox(
+              height: height * 108 / 640,
+              width: width * 101 / 360,
+              child: Image.asset("assets/images/logo.png"),
+            ),
+            SizedBox(height: height * 180 / 640),
+            const CircularProgressIndicator(
+              color: kPrimaryColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
