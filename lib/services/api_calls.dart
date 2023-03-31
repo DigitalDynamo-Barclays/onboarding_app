fd352d6515f6e24fd6b9d7323dac91fe99f76ff5
Tanishq2505 <tanishqa89@gmail.com>
2023-03-31 18:01:02 +0530

    api calls added ✨

---
+++ lib/services/api_calls.dart
@@ -0,0 +1,65 @@
+import 'package:barclays_onboarding/widgets/show_snack_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:barclays_onboarding/services/media_service.dart';
+
+class APICalls {
+  late final MediaService _mediaService;
+  late BuildContext _context;
+
+  APICalls(BuildContext context) {
+    _mediaService = MediaService();
+    _context = context;
+  }
+
+  sendPersonalInfo({
+    required String name,
+    required String dob,
+    required String block,
+    required String street,
+    required String city,
+    required String state,
+    required String zip,
+    required String country,
+  }) {
+    try {
+      var response = _mediaService.post(
+        '/personal-info',
+        {
+          "name": name,
+          "dob": dob,
+          "address": {
+            "block": block,
+            "street": street,
+            "city": city,
+            "state": state,
+            "zip": zip,
+            "country": country
+          }
+        },
+      );
+      return response;
+    } catch (e) {
+      showSnackBar(
+        _context,
+        e.toString(),
+        Colors.red,
+      );
+    }
+  }
+
+  sendContactInfo({required String phone, required String email}) {
+    try {
+      var response = _mediaService.post(
+        '/personal-info',
+        {"phone": phone, "email": email},
+      );
+      return response;
+    } catch (e) {
+      showSnackBar(
+        _context,
+        e.toString(),
+        Colors.red,
+      );
+    }
+  }
+}
